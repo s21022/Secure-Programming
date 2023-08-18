de2537ed28b3c5ce5d395aa357c69176f0e7f36a
s21022 <s21022@std.it-college.ac.jp>
2023-08-18 04:24:52 +0000

    secure programming

---
+++ 31/31-003.php
@@ -0,0 +1,19 @@
+<?php
+	$name = @$_POST['name'];
+	$mail = @$_POST['mail'];
+	$gender = @$_POST['gender'];
+?>
+<html>
+<head><title>確認</title><head>
+<body>
+<form action="31-004.php" method="POST">
+氏名:<?php echo htmlspecialchars($name, ENT_NOQUOTES, 'UTF-8'); ?><br>
+メールアドレス:<?php echo htmlspecialchars($mail, ENT_NOQUOTES, 'UTF-8'; ?><br>
+性別:<?php echo htmlspecialchars($gender, ENT_NOQUOTES, 'UTF-8');?><br>
+<input type="hidden" name="name" value="<?php echo htmlspecialchars($name, ENT_COMPAT, 'UTF-8'); ?>">
+<input type="hidden" name="mail" value="<?php echo htmlspecialchars($mail, ENT_COMPAT, 'UTF-8'); ?>">
+<input type="hidden" name="gender" value="<?php echo htmlspecialchars($gender, ENT_COMPAT, 'UTF-8'); ?>">
+<input type="submit" value="登録">
+</form>
+</body></html>
+
